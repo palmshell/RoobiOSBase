ac82fca0995c8029b55991b06088a29f83bc7d4b
shawn <shawn@palm.computer>
2024-05-15 09:58:07 +0800

    xorg.git

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ mount $EFI /mnt/boot
 # ------------------------ install base ----------------------------
 
 # base
-pacstrap -K /mnt base base-devel linux linux-firmware vulkan-intel intel-ucode sudo grub efibootmgr networkmanager openssh electron xorg-xinit  adobe-source-han-sans-cn-fonts noto-fonts adobe-source-han-sans-kr-fonts parted pigz usbutils vim nano lsof iperf3 stress bc net-tools alsa-utils bluez bluez-utils btrfs-progs gptfdisk ntfs-3g rsync bash-completion wget bind-tools hdparm smartmontools hdparm sysstat lvm2 mdadm tcpdump unzip timeshift gzip xz dmidecode python python-evdev python-pyserial libgpiod nginx libpulse
+pacstrap -K /mnt base base-devel linux linux-firmware vulkan-intel intel-ucode sudo grub efibootmgr networkmanager openssh
 genfstab -U /mnt >> /mnt/etc/fstab
 sed -i "s/^.*swap.*$//g" /mnt/etc/fstab
 sed -i "s/^.*ext4.*$//g" /mnt/etc/fstab
@@ -123,15 +123,18 @@ echo "enable systemctl..."
 arch-chroot /mnt systemctl enable systemd-timesyncd NetworkManager
 
 chmod -R  777 /mnt/home/ps/xorgproto-git/
-arch-chroot /mnt bash -c 'cd /home/ps/xorgproto-git && su ps -c "makepkg -si" '
+arch-chroot /mnt bash -c 'cd /home/ps/xorgproto-git && su ps -c "makepkg -si --noconfirm " '
 
 chmod -R  777 /mnt/home/ps/xorg-server-git/
-arch-chroot /mnt bash -c 'cd /home/ps/xorg-server-git && su ps -c "makepkg -si" '
+arch-chroot /mnt bash -c 'cd /home/ps/xorg-server-git && su ps -c "makepkg -si --noconfirm" '
 
-arch-chroot /mnt pacman -R base-devel git
+arch-chroot /mnt pacman -Sy -noconfirm  electron xorg-xinit  adobe-source-han-sans-cn-fonts noto-fonts adobe-source-han-sans-kr-fonts parted pigz usbutils vim nano lsof iperf3 stress bc net-tools alsa-utils bluez bluez-utils btrfs-progs gptfdisk ntfs-3g rsync bash-completion wget bind-tools hdparm smartmontools hdparm sysstat lvm2 mdadm tcpdump unzip timeshift gzip xz dmidecode python python-evdev python-pyserial libgpiod nginx libpulse
+
+arch-chroot /mnt pacman -R -noconfirm base-devel git
 
 echo "Clean up..."
-arch-chroot /mnt pacman -Scc
+arch-chroot /mnt pacman -Scc --noconfirm
+
 rm /mnt/var/cache/pacman/pkg/*
 rm /mnt/var/lib/pacman/sync/*.db
 
